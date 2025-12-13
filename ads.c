1e638921365a0e7ea54376f7c5fc8d6a78fbee5b
Gaurvi Garg <garggaurvi0@gmail.com>
2025-12-13 19:11:45 +0530

    gaurvi update

---
+++ ads.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num1 = 9;
+    int num2 = 5;
+    
+    int sum = num1 + num2;
+    
+    printf("The first number is: %d\n", num1);
+    printf("The second number is: %d\n", num2);
+    printf("The sum of %d and %d is: %d\n", num1, num2, sum);
+    
+    return 0;
+}
