96ad03cc1a441bfd655e0c426918a3ee40011cca
Sandhya <sandhyaramadoss18@gamil.com>
2025-12-13 12:16:12 +0530

    Added division logic to sum program

--- sum.c
+++ sum.c
@@ -5,6 +5,17 @@ void multiply_by_two(int *num)
     *num = (*num) * 2;
     printf("%d\n", *num);
 }
+void divide_by_two(int num)
+{
+    if (num != 0)
+    {
+        printf("After division: %d\n", num/2);
+    }
+    else
+    {
+        printf("Division not possible\n");
+    }
+}
 int main()
 {
     int a = 4;
@@ -15,4 +26,6 @@ int main()
     int sum = a + b + c + d + e;
     printf("%d\n", sum);
     multiply_by_two(&sum);
+    divide_by_two(sum);
+    return 0;
 }
