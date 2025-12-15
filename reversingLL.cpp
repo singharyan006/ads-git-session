bd96579899aaabf638b40e83a4ef204cdbb28718
NiranjanaKrishnaK <niranjanakumar2510@gmail.com>
2025-12-15 11:13:22 +0530

    Add reversingLL.cpp with linked list reversal program

---
+++ reversingLL.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+Node* reverseList(Node* head) {
+    Node* prev = nullptr;
+    Node* curr = head;
+    Node* next = nullptr;
+
+    while (curr != nullptr) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+void printList(Node* head) {
+    while (head != nullptr) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+
+    cout << "Original list: ";
+    printList(head);
+
+    head = reverseList(head);
+
+    cout << "Reversed list: ";
+    printList(head);
+
+    return 0;
+}
